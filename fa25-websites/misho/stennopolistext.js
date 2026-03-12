90fd3108a97a865e87e51a6a9970e23441f926a9
roopa vasudevan <roopakutty@gmail.com>
2026-03-12 11:09:21 -0400

    Add files via upload

---
+++ fa25-websites/misho/stennopolistext.js
@@ -0,0 +1,12 @@
+function manipulate(heading,text){
+    document.getElementById("quartiersHeading").innerHTML = heading;
+    document.getElementById("quartiers").innerHTML = text;
+    return false;
+}
+
+// function manipulate(heading,text){
+//     headimg = titles[head]
+//     document.getElementById("quartiersHeading").innerHTML = heading;
+//     document.getElementById("quartiers").innerHTML = text;
+//     return false;
+// }
