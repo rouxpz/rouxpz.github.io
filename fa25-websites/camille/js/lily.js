3f402621ca86c45cf6b0adcad30999c80a9d449a
roopa vasudevan <roopakutty@gmail.com>
2026-03-12 11:07:21 -0400

    Add files via upload

---
+++ fa25-websites/camille/js/lily.js
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+ <script>
+function myFunction() {
+  document.getElementById("button1").innerHTML = "You can email us at flowernibblersoap@gmail.com for commissions!";
+}
+</script>
+
+<p id="demo">*Notice</p>
+
+<button type="button" onclick="myFunction()">Click Here</button>
+}
